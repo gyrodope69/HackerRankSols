40eb4042c8cddde57a5d8e10b5a721828fd4cfec
oops-aman <b200054@nitsikkim.ac.in>
2022-10-11 00:46:25 +0530

    Sherlock and Array

---
+++ sherlock_and_array.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'balancedSums' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+ 
+// int sum(vector<int> arr, int pos) {
+//     int total = 0;
+//     for(int i = pos; i < arr.size(); i++)
+//         total += arr[i];
+        
+//     return total;
+// }
+
+string balancedSums(vector<int> arr) {
+    int n = arr.size();
+        if(n == 0) return "NO";
+        if(n == 1) return "YES";
+        
+        int rightSum = 0;
+        for(int x : arr)
+            rightSum += x;
+        
+        int leftSum = 0;
+        for(int x : arr)
+        {
+            if(leftSum == rightSum - x)
+                return "YES";
+            leftSum += x;
+            rightSum -= x;
+        }
+        
+        return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string T_temp;
+    getline(cin, T_temp);
+
+    int T = stoi(ltrim(rtrim(T_temp)));
+
+    for (int T_itr = 0; T_itr < T; T_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        string result = balancedSums(arr);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
